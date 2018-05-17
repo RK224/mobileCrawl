7b15d499cb1f19d0e1654a79ea4239cc107a641b
RK224 <rahulkhapre1998@gmail.com>
2018-05-18 01:23:10 +0530

    crawls shopclues to get data about mobile phones

---
+++ mobileBot/mobileBot/spiders/shopclues.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ShopcluesSpider(scrapy.Spider):
+    name = "shopclues"
+    allowed_domains = ["www.shopclues.com/mobiles-featured-store-4g-smartphone.html"]
+    start_urls = (
+        'http://www.www.shopclues.com/mobiles-featured-store-4g-smartphone.html/',
+    )
+
+    def parse(self, response):
+        pass
